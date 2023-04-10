00a22a217d044802b20797f183d415238d43d387
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-04-10 18:57:45 +0200

    add file creation on button

---
+++ src/lib/createFile.ts
@@ -0,0 +1,25 @@
+import { Plugin } from 'obsidian'
+
+/**
+ * Create a file with a name that's not reserved yet.
+ * @param that FantasyMapPlugin
+ * @param format file extension
+ * @param defaultContent content format
+ */
+export default async function createFile(
+    that: Plugin,
+    format: string,
+    defaultContent: string
+) {
+    const { adapter } = that.app.vault
+    let attempt = 0
+    const file_path = () =>
+        './Untitled' + (attempt > 0 ? ' ' + attempt : '') + '.' + format
+
+    while (await adapter.exists(file_path())) {
+        attempt++
+        console.log(attempt)
+    }
+
+    adapter.write(file_path(), defaultContent)
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import { Notice, Plugin } from "obsidian";
+import { Notice, Plugin } from 'obsidian'
+import createFile from './lib/createFile'
 
 // Remember to rename these classes and interfaces!
 
 export default class MapPlugin extends Plugin {
-    settings: {};
+    settings: {}
 
     async onload() {
-        this.addRibbonIcon('map', 'Create map fragment', () => new Notice('Hello'))
+        this.addRibbonIcon('map', 'Create map fragment', () =>
+            createFile(this, 'map', '{}')
+        )
     }
 
     onunload() {}
