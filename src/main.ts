ebd029c7a4f45dc32023a4c8ced6d6571e23d9c4
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-04-10 18:41:35 +0200

    remove leftover code

--- src/main.ts
+++ src/main.ts
@@ -6,23 +6,8 @@ export default class MapPlugin extends Plugin {
     settings: {};
 
     async onload() {
+
     }
 
     onunload() {}
 }
-
-// class SampleModal extends Modal {
-// 	constructor(app: App) {
-// 		super(app);
-// 	}
-
-// 	onOpen() {
-// 		const {contentEl} = this;
-// 		contentEl.setText('Woah!');
-// 	}
-
-// 	onClose() {
-// 		const {contentEl} = this;
-// 		contentEl.empty();
-// 	}
-// }
