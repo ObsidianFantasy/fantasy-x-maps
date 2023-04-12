d7df18e832ae661e6806f52f5fb7078a76bb22e9
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-04-12 13:43:48 +0200

    add map render class

---
+++ src/render/MapRender.ts
@@ -0,0 +1,26 @@
+import { MapView } from '../MapView'
+import { Polygons } from './Polygons'
+
+export class PolygonData {
+    /**
+     * [-10.000 meters; +10.000 meters]
+     */
+    height = 0
+
+    // TODO Biome, Nations, Climate+Rain+Wind+Pressure Generations, et c.
+}
+
+export class MapRender {
+    parent: MapView
+    polygons: Polygons
+    data: Map<number, PolygonData> // TODO architecture
+
+    constructor(parent: MapView) {
+        this.parent = parent
+        this.polygons = new Polygons(parent)
+    }
+
+    render() {
+        // TODO
+    }
+}
