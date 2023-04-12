1d3db1119badb8ce2c5bd34e45ffb8c087503100
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-04-12 09:13:52 +0200

    switch to display asymetric chunks

--- src/render/Polygons.ts
+++ src/render/Polygons.ts
@@ -8,13 +8,15 @@ export class Polygons {
     voronoi: Voronoi<Delaunay.Point>
 
     constructor() {
-        this.polygons = []
+        // this.polygons = []
 
-        for (let x = 0; x < 6; x++) {
-            for (let y = 0; y < 3; y++) {
-                this.polygons.push(new PolygonChunk(x, y))
-            }
-        }
+        // for (let x = 0; x < 6; x++) {
+        //     for (let y = 0; y < 3; y++) {
+        //         this.polygons.push(new PolygonChunk(x, y))
+        //     }
+        // }
+
+        this.polygons = [new PolygonChunk(0, 0), new PolygonChunk(0, 1), new PolygonChunk(2, 2)]
 
         this.calculateVoronoi()
     }
@@ -38,24 +40,31 @@ export class Polygons {
 
     calculateVoronoi() {
         this.delaunay = Delaunay.from(this.getSourcePoints())
-        this.voronoi = this.delaunay.voronoi([0, 0, 960, 500])
+        this.voronoi = this.delaunay.voronoi([0, 0, 6 * 160, 3 * 160])
     }
 
-    render(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
-        // ctx.strokeStyle = '#3f3f3f' // TODO set to a variable
-        // for (let i = 0; i < this.delaunay?.points.length; i++) {
-        //     ctx.beginPath()
-        //     // ctx.fillStyle = `rgba(255, 255, 255, ${Math.random()})`
-        //     ctx.fillStyle = `${i % 2 == 0 ? 'white' : 'black'}`
-        //     this.voronoi.renderCell(i, ctx)
-        //     ctx.fill()
-        // }
-
+    renderDebugPoints(ctx: CanvasRenderingContext2D) {
         ctx.fillStyle = 'white'
 
         const p = this.delaunay?.points
         for (let i = 0; i < p?.length; i += 2) {
             ctx.fillRect(p[i], p[i+1], 1, 1)
         }
+    }
+
+    renderPolygons(ctx: CanvasRenderingContext2D) {
+        ctx.strokeStyle = '#3f3f3f' // TODO set to a variable
+        for (let i = 0; i < this.delaunay?.points.length; i++) {
+            ctx.beginPath()
+            // ctx.fillStyle = `rgba(255, 255, 255, ${Math.random()})`
+            ctx.fillStyle = `${i % 2 == 0 ? 'white' : 'black'}`
+            this.voronoi.renderCell(i, ctx)
+            ctx.fill()
+        }
+    }
+
+    render(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+        this.renderPolygons(ctx)
+        // this.renderDebugPoints(ctx)
     }
 }
