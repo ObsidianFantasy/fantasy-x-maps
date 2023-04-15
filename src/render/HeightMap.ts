2f1575f392e265bd35af38295c3b85c4a8f3ed9e
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-04-15 17:43:41 +0200

    add height map class

---
+++ src/render/HeightMap.ts
@@ -0,0 +1,19 @@
+import { MultiPolygon, union } from 'polygon-clipping'
+import { MapView } from '../MapView'
+
+// TODO move height map render here
+
+/**
+ * Display the continental borders
+ */
+export class HeightMap {
+    polygons: MultiPolygon
+
+    recalculate(parent: MapView) {
+        // TODO
+    }
+
+    render(parent: MapView) {
+        // TODO
+    }
+}
