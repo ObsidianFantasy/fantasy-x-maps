c4ab2240aedf25fe386ce61b4218a185fb1770da
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-04-15 18:07:57 +0200

    refactor drawable

--- src/render/Drawable.ts
+++ src/render/Drawable.ts
@@ -1,7 +1,22 @@
+import { MapView } from '../MapView'
+
 /**
  * The interface that declares
  * behavior of render-able
  * components like LandBorder
  * and HeightMap
  */
-export interface Drawable {}
+export interface Drawable {
+    /**
+     * Recalculate is called on major changes to map (new tiles, mouse stroke, etc.)
+     */
+    recalculate(view: MapView): void
+
+    // TODO add 'moved' method to be called when zoom/ offset move occurs
+    // move(offset ?) ??
+
+    /**
+     * Render is called 60 times a second
+     */
+    render(view: MapView): void
+}
